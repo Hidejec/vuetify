ba01e500ac78f8805734de2af367ec944acc1802
Jerico Rillo(Hidejec) <jericorillo@gmail.com>
2019-02-12 15:34:59 +0800

    change tooltip directive name to VTooltip

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,13 +1,13 @@
 import Badge from './badge'
 import ClickOutside from './click-outside'
 import Ripple from './ripple'
-import Tooltip from './tooltip'
+import VTooltip from './tooltip'
 import Touch from './touch'
 
 export default {
   Badge,
   ClickOutside,
   Ripple,
-  Tooltip,
+  VTooltip,
   Touch
 }
